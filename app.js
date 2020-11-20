3266a94a1716a022318f487e21aea9a9825ac770
Felix Gutierrez <fgutierrez2191@icloud.com>
2020-11-20 17:13:53 -0500

    updated template

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const inquirer = require('inquirer');
-// const fs = require('fs');
-// const generatePage = require('./src/page-template.js');
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 // const pageHtml = generatePage(name, github);
 // fs.writeFile('./index.html', pageHtml, err => {
 //   if (err) throw err;
@@ -135,5 +135,11 @@ Add a New Project
 promptUser()
   .then(promptProject)
   .then(portfolioData => {
-    console.log(portfolioData);
+        const pageHTML = generatePage(portfolioData);
+
+     fs.writeFile('./index.html', pageHTML, err => {
+     if (err) throw new Error(err);
+
+      console.log('Page created! Check out index.html in this directory to see it!');
+     });
   });
